a3bab6f8ab86771946df98e55aad5a47e6fa5c5f
Vikas Sharma <viksr2202@gmail.com>
2026-06-16 11:34:49 +0530

    Fix CORS for Vercel deployment

--- app.js
+++ app.js
@@ -42,8 +42,21 @@ connectDB()
 app.use(helmet())
 
 // Enable CORS (allow frontend to call this API)
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://devmart-frontend.vercel.app",
+  process.env.CLIENT_URL
+].filter(Boolean)
+
 app.use(cors({
-  origin: process.env.CLIENT_URL || "*",
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
